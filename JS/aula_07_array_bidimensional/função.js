50265427fc694f9376423fe032bb02c2cc78dd1b
ArthurMurilo17 <Arthurmurilocorreiabarros17@gmail.com>
2024-05-13 21:48:05 -0300

    atividade

---
+++ JS/aula_07_array_bidimensional/função.js
@@ -0,0 +1,55 @@
+// function addNums(num1 = 1, num2 =1) {
+//     return num1 + num2;
+
+// }
+
+// //chamada a função addNums
+
+// let x = addNums(4,5);
+// console.log(x);
+
+// // chamada a função myfunc antes da declaração da função
+
+// let z = myFunc (4,5);
+// console.log(z);
+
+// function myFunc(num1,num2){
+//     return num1 * num2;
+// }
+
+// console.clear();
+
+// const hello = () => {
+//     return 'Olá Arrow Function';
+// };
+
+// //exemplo usando versão da addNums:
+
+// const addNums2 = (num1 =1, num2 = 1) => {
+//     return num1 +num2;
+// };
+
+// console.clear();
+
+// const thello = () => {
+
+//     return 'Olá Arrow Function!';
+
+
+
+// };
+
+console.log(hello); //retorna o objeto da função
+
+console.log(hello()); // executa a função e imprime a string no retern
+
+console.clear();
+
+const addnums2 = (num1 =1,num2=2)=> {
+
+    return num1+num2;
+};
+
+let soma = addNums2 (5,10);
+
+console.log(soma);
