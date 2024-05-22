83d15ab5c9881b7d802ee420045b5a53b3af1059
ArthurMurilo17 <Arthurmurilocorreiabarros17@gmail.com>
2024-05-21 21:48:11 -0300

    atividade quarry

---
+++ JS/aula_11_Get_elementos/aula_13_manipulando_dom/query.js
@@ -0,0 +1,15 @@
+//Método par mudar a cor do paragrafo
+const Clique = () => {
+    document.querySelector('#my-span').style.backgroundColor = '#CEFBBA';
+    };
+
+    const Clique2 = () => {
+        let myItem = document.querySelectorAll('.item');
+        myItem.forEach((item) => (item.style.backgroundColor = '#0000FF'));
+    
+            
+        };
+            
+        
+
+    
